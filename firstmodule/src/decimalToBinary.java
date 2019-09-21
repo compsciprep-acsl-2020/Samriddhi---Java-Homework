1073473bef40639b343cfe6d2034ae6f2da80cd7
samriddhikumar <samriddhi.kumar@yahoo.com>
2019-09-21 15:48:32 -0400

    Decimal to Binary

---
+++ firstmodule/src/decimalToBinary.java
@@ -0,0 +1,16 @@
+public class decimalToBinary {
+    public static void main(String[] args){
+        //8 4 2 1
+        int num = 256;
+        String fin = "";
+        while(num != 1){
+            int ad = num%2;
+            num = num/2;
+            fin = fin+ad;
+        }
+        fin = "1" + fin;
+        System.out.println(fin);
+
+
+    }
+}
